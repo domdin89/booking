88711a0cc74597a4c5a4ab766eb023916460da16
Domenico Di Nardo <domenico.dnrd@hotmail.com>
2023-09-05 11:13:08 +0200

    fix

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from services.models import Service
+from structures.models import Structure
+
 
 # Create your models here.
+class Reservation(models.Model):
+    #profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    structure = models.ForeignKey(Structure, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.service, self.structure, str(self.date)
